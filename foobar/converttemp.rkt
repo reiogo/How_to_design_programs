5de8dd2f0fd768e1c8a1cf0534756bf700ab7b58
rayogoda <ray.ogoda@evolany.com>
2025-07-04 18:22:30 +0900

    Updates

--- foobar/converttemp.rkt
+++ foobar/converttemp.rkt
@@ -1,15 +1,20 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname idk) (read-case-sensitive #t) (teachpacks ((lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
-(define (C f)
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname converttemp) (read-case-sensitive #t) (teachpacks ((lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+;Use a simple Number to represent Temperature
+;Number -> Number
+;Convert from Fahrenheit to Celsius
+(check-expect (f2c -40) -40)
+(check-expect (f2c 32) 0)
+(check-expect (f2c 212) 100)
+(define (f2c f)
   (* 5/9 (- f 32)))
 
 (define (convert in out)
   (write-file out
     (string-append
      (number->string
-      (C
+      (f2c
        (string->number
         (read-file in))))
      "\n")))
-(convert "sample.dat" 'stdout)
