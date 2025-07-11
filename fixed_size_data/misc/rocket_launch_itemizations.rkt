4e528bf7fded2d6dfeb99eebc2563f6e9e399321
rayogoda <ray.ogoda@evolany.com>
2025-07-11 18:29:28 +0900

    add update

--- fixed_size_data/misc/rocket_launch_itemizations.rkt
+++ fixed_size_data/misc/rocket_launch_itemizations.rkt
@@ -17,34 +17,36 @@
 ; graphical constants
 (define BACKG (empty-scene WIDTH HEIGHT))
 (define ROCKET (rectangle 5 30 "solid" "red"))
-(define ROCKET-CENTER (/ (image-height ROCKET) 2)) ; pixels
+(define ROCKET-HEIGHT (image-height ROCKET))
+(define ROCKET-CENTER (/ ROCKET-HEIGHT 2)) ; pixels
 
-;->Image
+
+;LRCD->Image
 ;auxiliary function that puts the rocket on the background
 ; in the appropriate place
 (check-expect
  (_rocket-place HEIGHT)
- (place-image ROCKET X-pos (- HEIGHT ROCKET-CENTER) BACKG))
+ (place-image ROCKET X-pos (- HEIGHT (+ HEIGHT ROCKET-CENTER)) BACKG))
 (check-expect
  (_rocket-place 10)
- (place-image ROCKET X-pos (- 10 ROCKET-CENTER) BACKG))
+ (place-image ROCKET X-pos (- HEIGHT (+ 10 ROCKET-CENTER)) BACKG))
 (define (_rocket-place x)
-  (place-image ROCKET X-pos (- x ROCKET-CENTER) BACKG))
+  (place-image ROCKET X-pos (- HEIGHT (+ x ROCKET-CENTER)) BACKG))
 
-; LRCD -> Image
+;LRCD -> Image
 ; renders the state as a resting or flying rocket
 (check-expect
  (show "resting")
- (_rocket-place HEIGHT))
+ (_rocket-place 0))
 (check-expect
- (show HEIGHT)
- (_rocket-place HEIGHT))
+ (show 0)
+ (_rocket-place 0))
 
 (check-expect
  (show -2)
  (place-image (text "-2" 20 "red")
               X-pos (* 3/4 WIDTH)
-              (_rocket-place HEIGHT)))
+              (_rocket-place 0)))
 (check-expect
  (show 53)
  (_rocket-place 53))
@@ -52,11 +54,11 @@
 (define (show x)
   (cond
     [(string? x)
-     (_rocket-place HEIGHT)]
+     (_rocket-place (- HEIGHT HEIGHT))]
     [(<= -3 x -1)
      (place-image (text (number->string x) 20 "red")
                   X-pos (* 3/4 WIDTH)
-                  (_rocket-place HEIGHT))]
+                  (_rocket-place 0))]
     [(>= x 0)
      (_rocket-place x)]))
 
@@ -71,13 +73,53 @@
 (check-expect (launch 33 "a") 33)
 (define (launch x ke)
   (cond
-    [(string? x) ...]
-    [(<= -3 x -1) ...]
-    [(>= x 0) ...]))
+    [(string? x) (if (string=? " " ke) -3 x)]
+    [(<= -3 x -1) x]
+    [(>= x (- HEIGHT HEIGHT)) x]))
 
 ; LRCD -> LRCD
 ; raises the rocket by YDELTA,
 ; if it is moving already
+(check-expect (fly "resting") "resting")
+(check-expect (fly -3) -2)
+(check-expect (fly -2) -1)
+(check-expect (fly -1) 0)
+(check-expect (fly 10) (+ 10 YDELTA))
+(check-expect (fly 22) (+ 22 YDELTA))
 (define (fly x)
-  x)
+  (cond
+    [(string? x) x]
+    [(<= -3 x -1) (if (= x -1) 0 (+ x 1))]
+    [(>= x 0) (+ x YDELTA)]))
+
+;LRCD -> Boolean
+;Ends when the rocket is out of sight
+(check-expect (end? "resting") #false)
+(check-expect (end? HEIGHT) #true)
+(check-expect (end? -2) #false)
+(check-expect (end? 15) #false)
+(define (end? x)
+  (cond
+    [(string? x) #false]
+    [(<= -3 x -1) #false]
+    [(= x HEIGHT) #true]
+    [else #false]))
+
+; LRCD -> LRCD
+(define (main1 s)
+  (big-bang s
+    [to-draw show]
+    [on-key launch]))
+
+
+; LRCD -> LRCD
+(define (main2 s)
+  (big-bang s
+    [to-draw show]
+    [on-key launch]
+    [on-tick fly]
+    [stop-when end?]))
+(main2 "resting")
+
+
 
