a64f1be72a99ebbc237e4d55bc793fa0e21819d9
Your Name <your.email@example.com>
2025-07-29 10:45:02 +0900

    Add predicates

--- fixed_size_data/chapter_6/predicates_sigs_coord_vanim.rkt
+++ fixed_size_data/chapter_6/predicates_sigs_coord_vanim.rkt
@@ -5,3 +5,76 @@
 ; Coordinate
 ; VAnimal
 
+(define-struct vcat [x happiness direction])
+(define-struct vcham [x happiness direction color])
+; A VAnimal is one of:
+; – a VCat (make-vcat X-Pos Happiness VCatDirection)
+; – a VCham (make-vcham X-Pos Happiness Direction Color)
+
+; Any -> Boolean
+; is a an element of the VAnimal collection
+(check-expect (VAnimal? (make-vcat 4 4 -5))
+              #true)
+(check-expect (VAnimal? (make-vcham 4 4 -5 "green"))
+              #true)
+(check-expect (VAnimal? "hi")
+              #false)
+(check-expect (VAnimal? 4)
+              #false)
+(check-expect (VAnimal? (circle 4 "solid" "red"))
+              #false)
+
+(define (VAnimal? a)
+  (cond
+    [(vcat? a) #true]
+    [(vcham? a) #true]
+    [else #false]))
+
+(define-struct tank [loc vel])
+; A SIGS is one of:
+; - (make-aim UFO TANK)
+; - (make-fired UFO Tank Missile)
+; interpretation represents the state of the space invader game
+
+; Any -> Boolean
+; is a an element of the SIGS collection?
+(check-expect
+ (SIGS?
+  (make-aim
+   (make-posn 4 5)
+   (make-tank 4 5)))
+ #true)
+(check-expect
+ (SIGS?
+  (make-fired
+   (make-posn 4 5)
+   (make-tank 4 5)
+   (make-posn 4 5)))
+ #true)
+(check-expect
+ (SIGS?
+  "hi")
+ #false)
+(check-expect
+ (SIGS?
+  10)
+ #false)
+(check-expect
+ (SIGS?
+  (circle 3 "solid" "red"))
+ #false)
+
+(define (SIGS? a)
+  (cond []
+        []
+        []))
+
+; A Coordinate is one of:
+; – a NegativeNumber
+; interpretation a point on the Y axis, distance from top
+
+; – a PositiveNumber
+; interpretation a point on the X axis, distance from left
+
+; – a Posn
+; interpretation a point in a scene, usual interpretation
