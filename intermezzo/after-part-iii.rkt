41bf1404335c07dc06e6f506ee1af98a0016eb7e
Your Name <your.email@example.com>
2025-08-22 16:46:46 +0900

    Add to intermezzo, scope & abstraction

---
+++ intermezzo/after-part-iii.rkt
@@ -0,0 +1,81 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname after-part-iii) (read-case-sensitive #t) (teachpacks ((lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp")) #f)))
+; scope
+
+(define (h x)
+  (f (* 2 x)))
+(define (f y)
+  (+ (* y y) 25))
+(define (g x)
+  (+ (f (+ x 1)) (f (- x 1))))
+
+
+(define (p1 x y)
+  (+ (* x y)
+     (+ (* 2 x)
+        (+ (* 2 y) 22))))
+
+(define (p2 x)
+  (+ (* 55 x) (+ x 11)))
+
+(define (p3 x)
+  (+ (p1 x 0)
+     (+ (p1 x 1) (p2 x))))
+
+
+; [List-of X] -> [List-of X]
+; creates a version of the given list that is sorted in descending order
+(define (insertion-sort alon)
+  (local ((define (sort alon)
+            (cond
+              [(empty? alon) '()]
+              [else (add (first alon) (sort (rest alon)))]))
+          (define (add an alon)
+            (cond
+              [(empty? alon) (list an)]
+              [else
+               (cond
+                 [(> an (first alon)) (cons an alon)]
+                 [else (cons (first alon) (add an (rest alon)))])])))
+    (sort alon)))
+
+(define (srt alon)
+  (local ((define (srt alon)
+            (cond
+              [(empty? alon) '()]
+              [else (add (first alon) (srt (rest alon)))]))
+          (define (add an alon)
+            (cond
+              [(empty? alon) (list an)]
+              [else
+               (cond
+                 [(> an (first alon)) (cons an alon)]
+                 [else (cons (first alon) (add an (rest alon)))])])))
+    (srt alon)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
