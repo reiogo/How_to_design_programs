08d9193edf5e41fc34762e79eb3fb25b06d78dd3
Your Name <your.email@example.com>
2025-09-01 16:21:58 +0900

    Add xml s-expression

--- intertwined_data/interpreting_expression.rkt
+++ intertwined_data/interpreting_expression.rkt
@@ -124,40 +124,38 @@
 
 (check-expect
  (parse '(+ 5 4)) (make-add 5 4))
-
 (check-expect
  (parse '(* 5 4)) (make-mul 5 4))
-
-(check-error
- (parse '(+ 5)) WRONG)
-
-(check-error
- (parse '(/ 5 3)) WRONG)
-
 (check-error
  (parse '(* 3 5 3)) WRONG)
-
 (check-expect
  (parse '(+ d 3)) (make-add 'd 3))
-
 (check-error
  (parse '(+ "d" 3)) "strings not allowed")
+(check-expect
+ (parse '(d 3)) (make-func 'd 3))
+(check-error
+ (parse '(d)) WRONG)
+(check-error
+ (parse '(d 3 4)) WRONG) 
 
 
 
 (define (parse s)
-  (local (; S-expr -> BSL-var-expr
+  (local (; S-expr -> BSL-func-expr
           (define (parse s)
             (cond
               [(atom? s) (parse-atom s)]
               [else (parse-sl s)]))
 
-          ; SL -> BSL-var-expr
+          ; SL -> BSL-func-expr
           (define (parse-sl s)
             (local ((define L (length s)))
               (cond
-                [(< L 3)
+                [(< L 2)
                  (error WRONG)]
+                [(and (= L 2) (symbol? (first s)))
+                 (make-func (first s) (parse (second s)))]
                 [(and (= L 3) (symbol? (first s)))
                  (cond
                    [(symbol=? (first s) '+)
@@ -414,25 +412,25 @@
 ; bsl-func-def* is [List-of BSL-func-def]
 
 (define-struct bsl-func-def [name parameter body])
-; a BSL-func-def is a structure
+; a BSL-func-def-old is a structure
 ; (make-bsl-func-def Symbol Symbol BSL-func-expr)
 ; interpretation (make-bsl-func-def n p exp)
 ; n is the name, p is the parameter, exp is the function body
 (define f (make-bsl-func-def 'f 'x (make-add 3 'x)))
 (define g (make-bsl-func-def 'g 'y (make-func 'f (make-mul 2 'y))))
 (define h  (make-bsl-func-def 'h 'v
-                       (make-add
-                        (make-func 'f 'v) (make-func 'g 'v))))
+                              (make-add
+                               (make-func 'f 'v) (make-func 'g 'v))))
 (define da-fgh
   (list
    (make-bsl-func-def 'f 'x (make-add 3 'x))
    (make-bsl-func-def 'g 'y (make-func 'f (make-mul 2 'y)))
    (make-bsl-func-def 'h 'v
-                       (make-add
-                        (make-func 'f 'v) (make-func 'g 'v)))))
+                      (make-add
+                       (make-func 'f 'v) (make-func 'g 'v)))))
 
 
- ; BSL-func-def* Symbol -> BSL-func-def
+; BSL-func-def* Symbol -> BSL-func-def
 ; retrieves the definition of f in da
 ; or signal "undefined function" if da does not contain one
 
@@ -475,12 +473,191 @@
                 (make-exp (add-right bfe) bfd*))]
               [(func? bfe)
                (local ((define spec-func (lookup-def bfd* (func-name bfe))))
-               (make-exp
-                (subst bfe (bsl-func-def-parameter spec-func)
-                       (make-exp (func-expr bfe) bfd*))
-                         bfd*))])))
+                 (make-exp
+                  (subst (bsl-func-def-body spec-func) (bsl-func-def-parameter spec-func)
+                         (make-exp (func-expr bfe) bfd*))
+                  bfd*))])))
     (eval-expression (make-exp bfe bfd*))))
 
+
+
+
+(define-struct def [name para body])
+; a Def is a structure
+; (make-def Symbol Symbol BSL-func-expr)
+; interpretation (make-def n p exp)
+; n is the name, p is the parameter, exp is the function body
+
+; see exercise 360
+; S-expr -> BSL-fun-def
+; creates representation of a BSL definition for s (if possible)
+
+(check-error (def-parse 1) WRONG)
+(check-error (def-parse '(+ 1 1) WRONG))
+(check-error (def-parse '(define (f x z) 3)) WRONG)
+(check-error (def-parse '(define 0 3)) WRONG)
+(check-error (def-parse '(define 1 1) WRONG))
+(check-error (def-parse '(define 0 3 3)) WRONG)
+(check-error (def-parse '(defi 0 3 3)) WRONG)
+(check-error (def-parse '(define (f 3) 3)) WRONG)
+(check-expect (def-parse '(define f 3)) (make-const 'f 3))
+(check-expect (def-parse '(define (f x) (+ 3 x)))
+              (make-def 'f 'x (make-add 3 'x)))
+
+
+(define (def-parse s)
+  (local (; S-expr -> BSL-fun-def
+          (define (def-parse s)
+            (cond
+              [(atom? s) (error WRONG)]
+              [else
+               (cond
+                 [(eq? (first s) 'define)
+                  (cond
+                    [(symbol? (second s)) (make-const (second s) (parse (third s)))]
+                    [(= (length s) 3) (head-parse (second s) (parse (third s)))]
+                    [else (error WRONG)])]
+                 [else (error WRONG)])]))
+          ; S-expr BSL-expr -> BSL-fun-def
+          (define (head-parse s body)
+            (cond
+              [(atom? s) (error WRONG)]
+              [else
+               (if (not (= (length s) 2))
+                   (error WRONG)
+                   (local ((define name (first s))
+                           (define para (second s)))
+                     (if (and (symbol? name) (symbol? para))
+                         (make-def name para body)
+                         (error WRONG))))])))
+    (def-parse s)))
+
+; SL -> BSL-func-def*
+; parse a sl into a list of b-func-defs (bsl-func-def*)
+
+(check-expect (da-parse '((define (f x) (* 3 x))
+                          (define (g x) (+ 3 x))))
+              (list
+               (make-def 'f 'x (make-mul 3 'x))
+               (make-def 'g 'x (make-add 3 'x))))
+
+(check-expect (da-parse '((define (f x) (* 3 x))
+                          (define g (+ 3 4))))
+              (list
+               (make-def 'f 'x (make-mul 3 'x))
+               (make-const 'g (make-add 3 4))))
+
+(define (da-parse sl)
+  (cond
+    [(empty? sl) '()]
+    [else
+     (cons
+      (def-parse (first sl))
+      (da-parse (rest sl)))]))
+
+(define-struct const [name body])
+; a Const is a structure
+; (make-const Symbol BSL-func-expr)
+; interpretation (make-def n exp)
+; n is the name, exp is the function body
+
+; A BSL-da-all is [List-of BSl-all]
+
+; A BSL-all is one of:
+; Number
+; Symbol
+; (make-add BSL-all BSL-all)
+; (make-mul BSL-all BSL-all)
+; (make-def symbol symbol BSL-all)
+; (make-const symbol BSL-all)
+
+(define ba0 (make-def 'f 'x (make-add 'x 4)))
+(define ba1 (make-const 'g (make-mul 4 3)))
+
+(define bda0 (list ba0 ba1))
+
+(define CONSTERR "no such constant definition")
+
+; BSL-da-all Symbol -> Const
+; find const definition in bda
+
+(check-error (lookup-con-def bda0 'h) CONSTERR)
+(check-expect (lookup-con-def bda0 'g) (make-const 'g (make-mul 4 3)))
+ 
+(define (lookup-con-def bda s)
+  (cond
+    [(empty? bda) (error CONSTERR)]
+    [else
+     (if 
+      (and (const? (first bda)) (symbol=? (const-name (first bda)) s))
+      (first bda)
+      (lookup-con-def (rest bda) s))]))
+
+(define DEFERR "no such function definition")
+
+; BSL-da-all Symbol -> Def
+; find func definition in bda
+
+(check-error (lookup-func-def bda0 'g) DEFERR)
+(check-expect (lookup-func-def bda0 'f)
+              (make-def 'f 'x (make-add 'x 4)))
+
+(define (lookup-func-def bda s)
+  (cond
+    [(empty? bda) (error DEFERR)]
+    [else
+     (if
+      (and (def? (first bda)) (symbol=? s (def-name (first bda))))
+      (first bda)
+      (lookup-func-def (rest bda) s))]))
+
+
+; BSL-func-expr BSL-da-all -> Number
+; Evaluate all expressions
+
+(check-expect (eval-all
+               (make-mul
+                2 (make-func 'f (make-add 2 3))) bda0) 18)
+(check-expect (eval-all
+               (make-mul
+                2 (make-func 'f (make-add 'g 3))) bda0) 38)
+(check-error (eval-all
+              (make-func 'h 2) DEFERR))
+(check-error (eval-all
+              (make-const 'i 2) CONSTERR))
+(check-error (eval-all
+              (make-mul 2 (make-const 'i 2)) CONSTERR))
+
+(define (eval-all bfe bda)
+  (local ((define (make-expr bfe bda)
+            (cond
+              [(number? bfe) bfe]
+              [(symbol? bfe) (const-body (lookup-con-def bda bfe))]
+              [(mul? bfe)
+               (make-mul
+                (make-expr (mul-left bfe) bda) (make-expr (mul-right bfe) bda))]
+              [(add? bfe)
+               (make-add
+                (make-expr (add-left bfe) bda) (make-expr (add-right bfe) bda))]
+              [(func? bfe)
+               (local ((define afunc (lookup-func-def bda (func-name bfe))))
+                 (make-expr
+                  (subst (def-body afunc) (def-para afunc)
+                         (make-expr (func-expr bfe) bda)) bda))])))
+    (eval-expression (make-expr bfe bda))
+    ))
+
+; S-expr SL -> Number
+; parses an s-exp and evals it
+
+(check-expect (eval-all-s-expr '(+ 3 (f 3 ))  '((define (f x) (+ 2 x))))
+              8)
+(check-expect (eval-all-s-expr '(+ g (f 3 )) '((define (f x) (+ 2 x))
+                                               (define g 8)))
+              13)
+
+(define (eval-all-s-expr s-exp sl)
+  (eval-all (parse s-exp) (da-parse sl)))
 
 
 
