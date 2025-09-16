57f6934f2cfcb774f2c26776bb6d6700986a6237
RO <raogoda2@gmail.com>
2025-09-16 20:54:01 +0900

    Add update

--- part_V/parsing.rkt
+++ part_V/parsing.rkt
@@ -14,11 +14,30 @@
 ; A Line is a [List-of 1String].
 
 ; (list "h" "i" "\n") -> (list (list "h" "i"))
-; '() -> (list (list ""))
-; (list "\n") -> (list (list "") (list ""))
+; '() -> '()
+; (list "\n") -> (list '() '())
 ; (list "\n" "\n")
-;      -> (list (list "") (list "") (list ""))
+;      -> (list '() '() '())
 
+(define NEWLINE "\n")
+
+; File -> Line
+; retrieves the prefix of afile up to the first occurence of NEWLINE
+(define (first-line afile)
+  (cond
+    [(empty? afile) '()]
+    [(string=? (first afile) NEWLINE) '()]
+    [else (cons (first afile) (first-line (rest afile)))]))
+
+; File -> Line
+; drops the suffix of afile behind the first occurence of NEWLINE
+(define (remove-first-line afile)
+  (cond
+    [(empty? afile) '()]
+    [(string=? (first afile) NEWLINE) (rest afile)]
+    [else (remove-first-line (rest afile))]))
+
+         
 ; File -> [List-of Line]
 ; converts a file into a list of lines
 
@@ -30,7 +49,50 @@
                     (list "f" "g" "h")))
 
 (define (file->list-of-lines afile)
+  (cond
+    [(empty? afile) '()]
+    [else
+     (cons (first-line afile)
+           (file->list-of-lines (remove-first-line afile)))]))
+; 4 generative questions:
+; if file is '()
+; file doesn't contain a line
+; else the file contains at least one "\n" or one 1String
+; these will be separated into one line, the rest is a new problem
+; cons the initial segment as a single line to the rest
+
+
+
+; A Token is a either a 1String or a String that consists of
+; lower-case letters and nothing else
+
+; Line -> String
+; Bundle the first word or 1String
+(define (bundle-first l)
+  (cond
+    [(not (string-lower-case? (first l))) ""]
+    [else
+     (string-append (first l) (bundle-first (rest l)))]))
+
+; Line -> Line
+; remove the first word/1String
+(define (remove-first l)
+  (cond
+    [(empty? l) '()]
+    [(not (string-lower-case? (first l))) l]))
+
+
+; Line -> [List-of Token]
+(define (tokenize l)
   '())
+
+; 4 generative questions:
+; if a line is empty
+; then the list of tokens is empty
+; else the file contains at least one 1String
+; consecutive letters will be bundled up into words and
+; non-letters will be as is or if they are whitespace, then removed
+; They will all be consed together
 
 
 
