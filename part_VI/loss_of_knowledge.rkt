e0a6f945d7ee6ce5da520b7a43cb6bc656455862
Your Name <your.email@example.com>
2025-09-22 16:57:41 +0900

    Add update from work

---
+++ part_VI/loss_of_knowledge.rkt
@@ -0,0 +1,68 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname loss_of_knowledge) (read-case-sensitive #t) (teachpacks ((lib "dir.rkt" "teachpack" "htdp") (lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "dir.rkt" "teachpack" "htdp") (lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+; Loss of Knowledge
+
+; Structural Processing ========================================
+
+; [List-of Number -> [List-of Number]
+; convert a list of relative distances to a list
+; of absolute distances
+; the first item on the list represents the distance to the origin
+
+(check-expect (relative->absolute '(50 40 70 30 30))
+              '(50 90 160 190 220))
+
+(define (relative->absolute l)
+  (cond
+    [(empty? l) '()]
+    [else (local ((define rest-of-l
+                    (relative->absolute (rest l)))
+                  (define adjusted
+                    (add-to-each (first l) rest-of-l)))
+                  (cons (first l) adjusted))]))
+
+; Number [List-of Number] -> [List-of Number]
+; add n to each number on l
+
+(check-expect (cons 50 (add-to-each 50 '(40 110 140 170)))
+              '(50 90 160 190 220))
+
+(define (add-to-each.v1 n l)
+  (cond
+    [(empty? l) '()]
+    [else (cons (+ (first l) n) (add-to-each n (rest l)))]))
+
+(define (add-to-each n l)
+  (map (lambda (l0) (+ n l0)) l))
+
+
+(define (test-speed size)
+(time (relative->absolute (build-list size add1))))
+
+; (test-speed 7000)
+(relative->absolute '(1 2 3))
+
+(define (relative->absolute/a l accu-dist)
+  (cond
+    [(empty? l) ...]
+    [else (local ((define tally (+ first l) accu-dist)))
+     (cons tally
+           (relative->absolute/a (rest l) tally))]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
